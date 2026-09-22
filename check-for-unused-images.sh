40612719998efc065af2ffd127cb09ecb2499dc6
Luke Short <ekultails@gmail.com>
2026-09-22 17:09:27 -0600

    fix(check-for-unused-images): use new directory structure

--- check-for-unused-images.sh
+++ check-for-unused-images.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for c in $(find high | cut -d/ -f2 | cut -d. -f1)
-    do if ! grep --quiet "[[:blank:]]${c}[[:blank:]]" CardData*.txt; then
+cd sets/setimages/general/
+for c in $(find . | cut -d/ -f2 | cut -d. -f1)
+    do if ! grep --quiet "[[:blank:]]${c}[[:blank:]]" ../../CardData*.txt; then
         echo "${c}.jpg is not used";
-        if ! grep --ignore-case --quiet "[[:blank:]]${c%%[0-9]*}" CardData*.txt; then
+        if ! grep --ignore-case --quiet "[[:blank:]]${c%%[0-9]*}" ../../CardData*.txt; then
             echo "${c}.jpg also does not have any similarly named cards"
         fi
     fi
 done
+cd -
